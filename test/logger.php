498c8f9060b7ef5b65d78400153a325cc112c0fa
PeterL73 <peter.leeneman+github@gmail.com>
2022-09-29 17:40:28 +0200

    add verbose with level

--- test/logger.php
+++ test/logger.php
@@ -20,7 +20,17 @@ Logger::writef('WARNING', 'Warning');
 Logger::writef('NOTICE', 'object','ERROR');
 Logger::writef('LEVEL', 'invalida logger level');
 Logger::setLevel('INVALID');
-
+Logger::infof('set level VERBOSE#40');
+Logger::setLevel('VERBOSE#40');
+Logger::verbosef('1', 'verbose level 1');
+Logger::verbosef('2', 'verbose level 2');
+Logger::verbosef('3', 'verbose level 3');
+Logger::infof('set level VERBOSE#2');
+Logger::setLevel('VERBOSE#2');
+Logger::verbosef('1', 'verbose level 1');
+Logger::verbosef('2', 'verbose level 2');
+Logger::verbosef('3', 'verbose level 3');
+Logger::infof('level 3 skipped');
 // test oud
 logger('ERROR', 'oud Error regel %d', 1);
 logger2('ERROR', 'oud Error regel %d', 2);
